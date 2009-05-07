5001a567f6593ed7a60b1477e52ff974944a5321
Eric Davis <edavis@littlestreamsoftware.com>
2009-05-07 16:57:36 -0700

    Allow @import statments if the class overrides allow_css_import_statment?.

--- lib/css_sanitize.rb
+++ lib/css_sanitize.rb
@@ -17,14 +17,21 @@ module CssSanitize
     # No backslashes allowed
     evil = [
       /(\bdata:\b|eval|cookie|\bwindow\b|\bparent\b|\bthis\b)/i, # suspicious javascript-type words
-      /behaviou?r|expression|moz-binding|@import|@charset|(java|vb)?script|[\<]|\\\w/i,
+      /behaviou?r|expression|moz-binding|@charset|(java|vb)?script|[\<]|\\\w/i,
       /[\<>]/, # back slash, html tags,
       /[\x7f-\xff]/, # high bytes -- suspect
       /[\x00-\x08\x0B\x0C\x0E-\x1F]/, #low bytes -- suspect
       /&\#/, # bad charset
     ]
+    evil << /@import/ unless allow_css_import_statment?
+
     evil.each { |regex| text = nil and break if no_comments =~ regex }
 
     text
+  end
+
+  # Should '@import' css statements be allowed?
+  def allow_css_import_statment?
+    false
   end
 end
